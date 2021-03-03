383c020f6f9f21292d93f25acc47d39b2b1b19b5
Figo <figoliu239@hotmail.com>
2021-03-03 23:17:44 +0800

    created stateful dialog box

--- lib/Userrating.dart
+++ lib/Userrating.dart
@@ -2,10 +2,12 @@
 
 //grade image can be local asset, image have to store online
 class Userrating {
+  final productname;
   final String name,image;
   final double star;
   final String comment;
   Userrating({
+    this.productname,
     this.name,
     this.image,
     this.star,
--- lib/pages/DetailUserrating/detail_userrating_screen.dart
+++ lib/pages/DetailUserrating/detail_userrating_screen.dart
@@ -9,17 +9,17 @@ class DetailUserrating extends StatelessWidget {
   Widget build(BuildContext context) {
     // todo: a function to search database by product.name -> create a list object with all comments
     List<Userrating> ratinglist = [
-      Userrating(name:"Figo Liu",image:"assets/images/tempUserpicture.jpg",star:5.0,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu2 very long nammmmmmmmmmmmmmmmmmmme",image:"assets/images/tempUserpicture.jpg",star:1,comment:"It taste shit"),
-      Userrating(name:"Figo Liu3",image:"assets/images/tempUserpicture.jpg",star:3.5,comment:"It taste ok"),
-      Userrating(name:"Figo Liu4",image:"assets/images/tempUserpicture.jpg",star:1.5,comment:"Long commentIt taste sweet ddkfsdfl sdljfkdj ldd k l sljsfksjf ks kjk j ljf lkfskjslkjiwo weojejfwjfo jwjejfijfef  foefijwifwiefefifjw  wfojfw"),
-      Userrating(name:"Figo Liu5",image:"assets/images/bread.jpg",star:4.1,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
-      Userrating(name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/tempUserpicture.jpg",star:5.0,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu2 very long nammmmmmmmmmmmmmmmmmmme",image:"assets/images/tempUserpicture.jpg",star:1,comment:"It taste shit"),
+      Userrating(productname:product.name,name:"Figo Liu3",image:"assets/images/tempUserpicture.jpg",star:3.5,comment:"It taste ok"),
+      Userrating(productname:product.name,name:"Figo Liu4",image:"assets/images/tempUserpicture.jpg",star:1.5,comment:"Long commentIt taste sweet ddkfsdfl sdljfkdj ldd k l sljsfksjf ks kjk j ljf lkfskjslkjiwo weojejfwjfo jwjejfijfef  foefijwifwiefefifjw  wfojfw"),
+      Userrating(productname:product.name,name:"Figo Liu5",image:"assets/images/bread.jpg",star:4.1,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
+      Userrating(productname:product.name,name:"Figo Liu",image:"assets/images/bread.jpg",star:3.4,comment:"It taste sweet"),
     ];
     return Scaffold(
       appBar: buildAppBar(product.name),
