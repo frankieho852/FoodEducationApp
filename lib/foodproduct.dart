8682381975746e3d3901e01db6ca93b992a3c58b
Figo <figoliu239@hotmail.com>
2021-03-21 00:49:03 +0800

    update foodproduct object

--- lib/foodproduct.dart
+++ lib/foodproduct.dart
@@ -1,8 +1,16 @@
 // the following values are total value, not /100g value
-// OCR process: Scan -> input total volume -> calculate total nutrients values -> store
+// Data input process: Take photo of a food product -> Barcode Scan -> ORC scan ingredient list -> input total volume -> input per100 gram values(ocr table not working)
+// ->calculate total nutrients values by 100values and total volume -> calculate grade ->store
+
 class FoodProduct {
-  String name;
-  String category;
+  String name; //used for search product by name
+  String category; //used in alternatives
+  String barcode; //used for search product by barcode
+  String image;
+  String grade;
+
+  double volumeOrweight;
+
   double energy = 0;
   double protein = 0;
   double totalFat = 0;
@@ -12,6 +20,7 @@ class FoodProduct {
   double dietarytFibre = 0;
   double sugars = 0;
   double sodium = 0;
+
   double energy_100 = 0;
   double protein_100 = 0;
   double totalFat_100 = 0;
@@ -21,36 +30,36 @@ class FoodProduct {
   double dietarytFibre_100 = 0;
   double sugars_100 = 0;
   double sodium_100 = 0;
-  String image;
-  String grade;
-  List<String> ingredients;
-  int calories;
-  double star=0; //average rating by users
 
-  FoodProduct(
-      {this.name,
-      this.category,
-      this.energy,
-      this.protein,
-      this.totalFat,
-      this.saturatetedFat,
-      this.transFat,
-      this.totalCarbonhydrates,
-      this.dietarytFibre,
-      this.sugars,
-      this.sodium,
-      this.energy_100,
-      this.protein_100,
-      this.totalFat_100,
-      this.saturatetedFat_100,
-      this.transFat_100,
-      this.totalCarbonhydrates_100,
-      this.dietarytFibre_100,
-      this.sugars_100,
-      this.sodium_100,
-      this.image,
-      this.ingredients,
-      this.grade,
-      this.star,
-      this.calories});
+  List<String> ingredients;
+
+  double star = 0; //average rating by users, default 0 when no user ratting, whenever user update a userratting, this value have to update
+
+  FoodProduct({
+    this.name,
+    this.category,
+    this.barcode,
+    this.grade,
+    this.image,
+    this.energy,
+    this.protein,
+    this.totalFat,
+    this.saturatetedFat,
+    this.transFat,
+    this.totalCarbonhydrates,
+    this.dietarytFibre,
+    this.sugars,
+    this.sodium,
+    this.energy_100,
+    this.protein_100,
+    this.totalFat_100,
+    this.saturatetedFat_100,
+    this.transFat_100,
+    this.totalCarbonhydrates_100,
+    this.dietarytFibre_100,
+    this.sugars_100,
+    this.sodium_100,
+    this.ingredients,
+    this.star,
+  });
 }
