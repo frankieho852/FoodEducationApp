fa53c3c2c2ae98911dbba4f42c70ea3361ec28b0
samtang2a <78518770+samtang2a@users.noreply.github.com>
2021-04-21 18:42:53 +0800

    Revert "update detail alternative screen"
    This reverts commit 8c63d09a

--- lib/pages/DetailAlternative/detail_alternative_screen.dart
+++ lib/pages/DetailAlternative/detail_alternative_screen.dart
@@ -80,7 +80,6 @@ class _DetailAlternativeState extends State<DetailAlternative> {
         for (DocumentSnapshot document in value.docs) {
           altproductslist.add(AlternativeProduct(
               name: document.data()["name"], image: document.data()["image"]));
-          print("adding in alt page");
         }
       });
     } on StateError catch (e) {
