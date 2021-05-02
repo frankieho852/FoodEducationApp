01e7ac57f04f70101b80e3a6561026b74d466257
samtang2a <78518770+samtang2a@users.noreply.github.com>
2021-05-03 02:02:37 +0800

    Revert "auth state error fixed"
    This reverts commit aba7c8d4

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -80,6 +80,7 @@ class AuthService {
   }
 
   void checkAuthStatus() async {
+    log("Start App now");
     try {
       // Wait for Firebase to initialize and set `_initialized` state to true
       await Firebase.initializeApp();
