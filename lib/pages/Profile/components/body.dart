7c02d88a6037c923496b004b9dc138464106bb0a
samtang2a <78518770+samtang2a@users.noreply.github.com>
2021-05-05 22:23:05 +0800

    remove rubbish

--- lib/pages/Profile/components/body.dart
+++ lib/pages/Profile/components/body.dart
@@ -60,22 +60,8 @@ class _BodyState extends State<Body> {
       children: [
         ProfilePic(),
         SizedBox(height: 20),
-        ProfileMenu(
-          text: "My Account",
-          icon: "assets/icons/User Icon.svg",
-          press: () => {},
-        ),
 
-        ProfileMenu(
-          text: "Settings",
-          icon: "assets/icons/Settings.svg",
-          press: () {},
-        ),
-        ProfileMenu(
-          text: "Help Center",
-          icon: "assets/icons/Question mark.svg",
-          press: () {},
-        ),
+
         ProfileMenu(
           text: "Log Out",
           icon: "assets/icons/Log out.svg",
