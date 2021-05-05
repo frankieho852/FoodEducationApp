1c4117077ce341d8b3969ea76044095b9d4dd766
Po-Jui-Lin <plinag@connect.ust.hk>
2021-05-05 17:56:07 +0800

    blur out item images for aesthetics purposes_2

--- lib/pages/Reward/model/item.dart
+++ lib/pages/Reward/model/item.dart
@@ -28,7 +28,7 @@ List<Item> items = [
     id: 3,
     price: 400,
     title: "Reusable Bamboo Lunch Box",
-    image: "assets/images/Item_3.jpg",
+    image: "assets/images/Item_3.png",
     description:
         "This eco friendly lunchbox has a capacity of 700ml and is made from both natural bamboo and bamboo fibre, with an elastic band and food-grade silicone seal. The bamboo has naturally anti-bacterial properties and it's BPA-free.",
   ),
